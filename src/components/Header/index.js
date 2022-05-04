47309eb9648f7b71f3f22bb7cfb109a26d422205
Lou-Nardelli <nardelli.lou@gmail.com>
2022-05-04 16:08:16 +0200

    header_trying nav component  with MUI

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,49 @@
 // == Import : npm
-// import './header.scss';
+import { NavLink } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@material-ui/icons/Menu";
+
+import {
+  Toolbar,
+  Container,
+  Hidden,
+  IconButton,
+} from '@mui/material';
+
+// == Import : local
+import ohisseLogo from 'src/assets/img/logo-complet-bicolor.png';
+import './header.scss';
 
 // == Composant
 function Header() {
+  const navLinks = [
+    { name: 'Carte', href: '/' },
+    { name: 'Ajout de spot', href: '/spot' },
+    { name: 'Salles intérieur', href: '' },
+    { name: 'Spots extérieurs', href: '' },
+  ];
+
   return (
-    <div className="header">
-      header
-    </div>
+    <AppBar position="sticky">
+      <Toolbar className="header">
+        <Container maxWidth="md">
+          <div>
+            <img className="logo" src={ohisseLogo} alt="ohisse logo" />
+            <Hidden xsDown>
+              {navLinks.map((item) => (
+                <NavLink to={item.href}>{item.name}</NavLink>
+              ))}
+            </Hidden>
+            <Hidden smUp>
+              <IconButton>
+                <MenuIcon />
+              </IconButton>
+            </Hidden>
+          </div>
+        </Container>
+      </Toolbar>
+    </AppBar>
   );
 }
 
