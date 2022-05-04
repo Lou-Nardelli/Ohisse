8abaa2f438ce7bf084154f9ee0f2031c614a404d
gillenlaxalt <flikflok64@gmail.com>
2022-05-04 16:01:43 +0200

    Put content on footer, and Links without paths. Began to style it

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,28 @@
 // == Import : npm
-// import './footer.scss';
+import { Link } from 'react-router-dom';
+
+// == Import : local
+import logoSimple from 'src/assets/img/logo-simple-bicolor.png';
+import './footer.scss';
 
 // == Composant
 function Footer() {
   return (
     <div className="footer">
-      footer
+      <div className="footer__socials">
+        <img src={logoSimple} alt="logo simple ohisse" />
+      </div>
+      <div className="footer__about">
+        A propos de O'Hisse
+      </div>
+      <div className="footer__links">
+        <Link className="footer-link" to="">L'équipe</Link>
+        <Link className="footer-link" to="">Mentions légales</Link>
+        <Link className="footer-link" to="">Plan du site</Link>
+      </div>
+      <div className="footer__copyright">
+        Proudly remotely made in France @2022 - ohisse.fr
+      </div>
     </div>
   );
 }
