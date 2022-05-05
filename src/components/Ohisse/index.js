5ff55a92bc2d72a653989506f9a1d5753880c275
Lou-Nardelli <nardelli.lou@gmail.com>
2022-05-05 16:16:33 +0200

    develop_modifications

--- src/components/Ohisse/index.js
+++ src/components/Ohisse/index.js
@@ -2,6 +2,7 @@
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
 import Home from '../Home';
+
 import './ohisse.scss';
 
 // == Composant
@@ -9,7 +10,6 @@ function Ohisse() {
   return (
     <div className="ohisse">
       <Header />
-      <img src={ohisseLogo} className="ohisse__logo" alt="ohisse logo" />
 
       <h1>Composant : Ohisse</h1>
       <Home />
