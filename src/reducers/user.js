f0479944086ef8c3a1c691775b5ff3d34a1280e9
Lou-Nardelli <nardelli.lou@gmail.com>
2022-05-09 15:35:54 +0200

    connexion_ controlled component in writing

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,3 +1,5 @@
+import { CHANGE_FIELD } from '../actions/user';
+
 export const initialState = {
   list: [],
   inputEmail: '',
@@ -6,6 +8,11 @@ export const initialState = {
 
 const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
+    case CHANGE_FIELD:
+      return {
+        ...state,
+        [action.name]: action.value,
+      };
     default:
       return state;
   }
