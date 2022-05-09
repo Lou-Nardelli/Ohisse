182b23fe69a6b5fc8286e4b32cfd07bb3047e1dc
gillenlaxalt <flikflok64@gmail.com>
2022-05-09 10:02:35 +0200

    homepage-map_Customized icons on the map and styled it

--- src/components/Home/HomeMap/icon.js
+++ src/components/Home/HomeMap/icon.js
@@ -1,8 +1,11 @@
+/* eslint-disable global-require */
 import L from 'leaflet';
 
 const ohisseIcon = new L.Icon({
   iconUrl: require('src/assets/img/logo-simple-bleu.png'),
-  iconSize: new L.Point(30),
+  iconSize: [25],
+  iconAnchor: [13, 19],
+  popupAnchor: [0, -29],
   className: 'leaflet-div-icon',
 });
 
