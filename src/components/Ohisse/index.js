be5c2eb7bda14f96abf1a2fad3e37e84639a45f2
Lou-Nardelli <nardelli.lou@gmail.com>
2022-05-09 11:28:33 +0200

    spots componant props

--- src/components/Ohisse/index.js
+++ src/components/Ohisse/index.js
@@ -1,3 +1,5 @@
+import { useSelector } from 'react-redux';
+
 // == Import
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
@@ -9,12 +11,24 @@ import './ohisse.scss';
 
 // == Composant
 function Ohisse() {
+  // all spots (from state)
+  const spots = useSelector((state) => state.spots.listSpots);
+  // filter to get a array of interior spots
+  const spotsInterior = spots.filter((item) => item.type === 'interieur');
+  // filter to get a array of outdoor spots
+  const spotsOutdoor = spots.filter((item) => item.type === 'exterieur');
   return (
     <div className="ohisse">
       <Header />
       <Home />
       <SpotPage />
-      <Spots />
+      {
+        // depending on the route,
+        // either the list of spots interior or the list of outdoor spots
+        // will be displayed
+      }
+      <Spots title="Salles intérieures" listSpots={spotsInterior} />
+      <Spots title="Spots extérieurs" listSpots={spotsOutdoor} />
       {/* Routes */
        /* Home url / */
        /* User url /user */
--- src/components/Spots/index.js
+++ src/components/Spots/index.js
@@ -1,14 +1,40 @@
+// ext
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+
 // composants
+import { Link } from 'react-router-dom';
 import Card from '../Card';
 // styles
 import './spots.scss';
 
-function Spots() {
+// componant receives a title and a filtered array
+function Spots({ listSpots, title }) {
   return (
     <div className="spots">
-      <Card />
+      <h2 className="spots__title">{title}</h2>
+      <div className="spots__cards">
+        {
+          listSpots.map((item) => (
+            <Link key={item.id} to="">
+              <Card
+                key={item.id}
+                name={item.name}
+                city={item.city}
+                picture={item.picture}
+              />
+            </Link>
+          ))
+        }
+      </div>
+
     </div>
   );
 }
+
+Spots.propTypes = {
+  title: PropTypes.string.isRequired,
+  listSpots: PropTypes.array.isRequired,
+};
 
 export default Spots;
