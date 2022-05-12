62ef1fa59df49f170734d846f5b63a86b702ef73
romain-bodiguel <romain.bodiguel@gmail.com>
2022-05-12 10:28:30 +0200

    404_adding styles to the page, page done

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -1,7 +1,15 @@
+// import
+import { Link } from 'react-router-dom';
+
+// Styles
+import './error.scss';
+
 function Error() {
   return (
     <div className="error">
-      ça c'est une 404 bb
+      <h1 className="error__title">Tu t'es trompé de voie, il va falloir aller ailleurs !</h1>
+      <p className="error__content">404</p>
+      <button type="button" className="error__button"><Link to="/">Retour à l'accueil</Link></button>
     </div>
   );
 }
