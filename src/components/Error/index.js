99e5568322f6f0472de28bcc98228e9176de917f
romain-bodiguel <romain.bodiguel@gmail.com>
2022-05-12 11:05:20 +0200

    404_adding gif in the 404 page

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -2,13 +2,17 @@
 import { Link } from 'react-router-dom';
 
 // Styles
+import failGif from 'src/assets/img/wall-climbing-jump.gif';
 import './error.scss';
 
 function Error() {
   return (
     <div className="error">
       <h1 className="error__title">Tu t'es trompé de voie, il va falloir aller ailleurs !</h1>
-      <p className="error__content">404</p>
+      <div className="error__content">
+        <p className="error__content--title">404</p>
+        <img className="error__content--gif" src={failGif} alt="Fail climbing gif" />
+      </div>
       <button type="button" className="error__button"><Link to="/">Retour à l'accueil</Link></button>
     </div>
   );
