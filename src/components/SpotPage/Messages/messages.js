38c685bdd0c1da42c3122acce0c1721f314d8120
romain-bodiguel <romain.bodiguel@gmail.com>
2022-05-17 17:33:07 +0200

    messages_minor changes on messages component

--- src/components/SpotPage/Messages/messages.js
+++ src/components/SpotPage/Messages/messages.js
@@ -1,23 +1,28 @@
-import { useSelector } from 'react-redux';
-import Message from '../Message/message';
+// import { useSelector } from 'react-redux';
+// import Message from '../Message/message';
 
 // styles
 import './messages.scss';
 
 function Messages() {
-  const messages = useSelector((state) => state.messages);
+  // const messages = useSelector((state) => state.messages);
 
-  console.log(messages);
+  // console.log(messages);
 
   return (
     <div className="messages">
-      {
+      {/* {
         messages.map(
           // on déverse l'ensemble des propriétés de message
           // en tant que props de Message
           (message) => <Message key={message.id} {...message} />,
         )
-      }
+      } */}
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
     </div>
   );
 }
