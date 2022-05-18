55b0b56686d5f1fa15179ac9220066d1a776164e
romain-bodiguel <romain.bodiguel@gmail.com>
2022-05-18 11:06:38 +0200

    messages_formated post date added on messages

--- src/components/SpotPage/Messages/Message/index.js
+++ src/components/SpotPage/Messages/Message/index.js
@@ -4,16 +4,22 @@ import PropTypes from 'prop-types';
 // styles
 import './message.scss';
 
-function Message({ pseudo, content }) {
+function Message({ pseudo, content, updated_at }) {
   // const isMine = useSelector((state) => state.currentAuthor === author);
 
   // on applique toujours la classe message
   // et la classe message-mine seulement si isMine vaut vrai
   // const className = classNames('message', { 'message--mine': isMine });
 
+  const dateFormat = Intl.DateTimeFormat('fr-FR', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  });
+
   return (
     <div className="message">
-      <div className="message__author">{pseudo} - le 24/09/1996</div>
+      <div className="message__author">{pseudo} - {dateFormat.format(Date.parse(updated_at))}</div>
       <div className="message__content">{content}</div>
     </div>
   );
@@ -22,6 +28,7 @@ function Message({ pseudo, content }) {
 Message.propTypes = {
   pseudo: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
+  updated_at: PropTypes.string.isRequired,
 };
 
 export default Message;
