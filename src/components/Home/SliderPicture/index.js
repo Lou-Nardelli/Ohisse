412b766d6f147ef3d7b8b46dea707ac5d7619b2e
romain-bodiguel <romain.bodiguel@gmail.com>
2022-05-23 14:50:01 +0200

    debug_changing pictures on home slider, fixing key prop on images

--- src/components/Home/SliderPicture/index.js
+++ src/components/Home/SliderPicture/index.js
@@ -1,13 +1,9 @@
 // == Import : npm
-import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Carousel } from 'react-responsive-carousel';
 import climbing1 from 'src/assets/img/slider/climbing1.jpg';
 import climbing2 from 'src/assets/img/slider/climbing2.jpg';
 import climbing3 from 'src/assets/img/slider/climbing3.jpg';
 import climbing4 from 'src/assets/img/slider/climbing4.jpg';
-
-// == Import : local
-import { nextIndex, previousIndex } from '../../../actions/spots';
 
 // styles
 import './sliderPicture.scss';
@@ -20,27 +16,25 @@ const picture = [
 ];
 
 function SliderPicture() {
-  const dispatch = useDispatch();
-  const currentIndex = useSelector((state) => state.spots.currentIndexSliderPicture);
-  const currentPicture = picture[currentIndex];
-
-  const handleNext = () => {
-    // console.log('i want to see the next picture');
-    dispatch(nextIndex());
-  };
-
-  const handlePrevious = () => {
-    // console.log('i want to see the previous picture');
-    dispatch(previousIndex());
-  };
-
   return (
     <div className="sliderPicture">
-      <button type="button" className="sliderPicture__button--previous" aria-label="previous" onClick={handlePrevious}> &lt; </button>
-      <Link to="">
-        <img className="sliderPicture__picture" src={currentPicture} alt="" />
-      </Link>
-      <button type="button" className="sliderPicture__button--next" aria-label="next" onClick={handleNext}> &gt; </button>
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showArrows={false}
+        showIndicators={false}
+        showStatus={false}
+        showThumbs={false}
+        stopOnHover={false}
+        interval={6000}
+        transitionTime={0}
+      >
+        {
+          picture.map((item) => (
+            <img key={item.indexOf} className="sliderPicture__picture" src={item} alt="" />
+          ))
+        }
+      </Carousel>
     </div>
   );
 }
